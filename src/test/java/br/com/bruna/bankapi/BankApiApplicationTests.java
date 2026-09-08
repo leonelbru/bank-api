ef5d2a59843216f93be95e08871b085ba7c2622b
Bruna Cristina <leonelxxb@gmail.com>
2026-09-08 13:46:11 -0300

    chore: initialize Spring Boot project with PostgreSQL Docker setup

---
+++ src/test/java/br/com/bruna/bankapi/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruna.bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
